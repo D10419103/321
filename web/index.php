480c0da6c8d624382fb6be1acecf12036ee0a625
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-23 16:17:48 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -34,7 +34,7 @@ foreach ($client->parseEvents() as $event) {
                 	$source=$event['source'];
               	      	$type = $source['type']; 
               	      	$id=$source['userId'];
-			$displayName=$source['$displayName'];
+			$displayName=$message['$displayName'];
                   	$roomid=$source['roomId'];
              	       	$groupid=$source['groupId'];
 			date_default_timezone_set('Asia/Taipei');
@@ -45,7 +45,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid. $displayName
+                                'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid. "\n" . $displayName
                             )	
                         )
                     	));			
