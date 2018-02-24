904b01527243852caa2400e64a1285da0fdc339c
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-24 20:26:41 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -33,7 +33,7 @@ foreach ($client->parseEvents() as $event) {
                 	$m_message = $message['text'];
                 	$source=$event['source'];
               	      	$id=$source['userId'];
-			$UserInfo=$source['UserInfo'];
+			$UserInfo=$message['UserInfo'];
 			$displayName=$UserInfo['displayName'];
                   	$roomid=$source['roomId'];
              	       	$groupid=$source['groupId'];
