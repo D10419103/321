3a162ecd9717749eebe9fe8a1afa223edd10fe9f
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-26 19:46:50 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -110,15 +110,20 @@ foreach ($client->parseEvents() as $event) {
         )
                     	));	
 			}else if (strtolower($message['text']) == "location" || $message['text'] == "地址" || $message['text'] == "位置"){
+				
+				
+				$latitude=$message['latitude'];
+                   	$longitude=$message['longitude'];
+			$address = $message['address'];
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
         'messages' => array(
             array(
                 'type' => 'location', // 訊息類型 (位置)
                 'title' => 'Example location', // 回復標題
-                'address' => '台灣高雄市三民區大昌一路 98 號 (立志中學)', // 回復地址
-                'latitude' => 22.653742, // 地址緯度
-                'longitude' => 120.32652400000006 // 地址經度
+                'address' => $address, // 回復地址
+                'latitude' => $longitude, // 地址緯度
+                'longitude' => $latitude // 地址經度
             )
         )
     ));
@@ -132,7 +137,7 @@ foreach ($client->parseEvents() as $event) {
 			$title=$message['title'];
                    	$latitude=$message['latitude'];
                    	$longitude=$message['longitude'];
-			$m_message = $message['address'];
+			$address = $message['address'];
                 	if($m_message!="")
                 	{
                 		$client->replyMessage(array(
@@ -140,7 +145,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $m_message . "\n". $longitude . "\n" . $latitude ."\n". $id 
+                                'text' => $address . "\n". $longitude . "\n" . $latitude ."\n". $id 
                             ),
                         ),
                     	));
