228000ccc3c27b35fb09818ccb85c60b2ace515d
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-26 21:25:17 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -33,8 +33,8 @@ foreach ($client->parseEvents() as $event) {
                 	$m_message = $message['text'];
                 	$source=$event['source'];
               	      	$id=$source['userId'];
-			$profile = $events['$profile'];
-			$displayName=$events['displayName'];
+			$profile = $source['$profile'];
+			$displayName=$profile['displayName'];
                   	$roomid=$source['roomId'];
              	       	$groupid=$source['groupId'];
 			date_default_timezone_set('Asia/Taipei');
