f47178318296d10f02df7e6ddbe5673d35ff3cda
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-01 10:42:35 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -39,7 +39,7 @@ foreach ($client->parseEvents() as $event) {
 			date_default_timezone_set('Asia/Taipei');
 			    
 			    
-$res = $client->getProfile('$id');
+$res = $client->getProfile('U8acc7f611c6f853ac53e1a474bd77c92');
 if ($m_message=="1") {
     $profile = $res->getJSONDecodedBody();
     $displayName = $profile['displayName'];
