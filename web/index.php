47d34796dcb247b648da44ed8a13136c87bb2fb4
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-01 11:08:27 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -24,6 +24,13 @@ $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+LineApiResponse verifyResponse = lineApiClient.getProfile();
+
+global $displayName = apiResponse.getResponseData().getDisplayName();
+global $pictureURL = apiResponse.getResponseData().getPictureUrl().toString();
+global $userID = apiResponse.getResponseData().getUserId().toString();
+global $statusMessage = apiResponse.getResponseData().getUserId();
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
@@ -33,7 +40,7 @@ foreach ($client->parseEvents() as $event) {
                 	$m_message = $message['text'];
                 	$source=$event['source'];
               	      	$id=$source['userId'];
-			$displayName = $contacts['displayName'];				
+			//$displayName = $contacts['displayName'];				
                   	$roomid=$source['roomId'];
              	       	$groupid=$source['groupId'];
 			date_default_timezone_set('Asia/Taipei');
