49cd4407e1520358bea5e147648eecddc04625ee
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-03 19:48:28 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -47,7 +47,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid. "\n" . $displayName"\n" . $hwid
+                                'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid. "\n" . $displayName ."\n" . $hwid
                             )	
                         )
                     	));			
