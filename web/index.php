16a54869644934c208a3fec939292289b9e50d6b
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-06 15:36:36 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -25,6 +25,18 @@ $channelSecret = getenv('LINE_CHANNEL_SECRET');
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('d94WAvqAJBWRXZ3pmnlejuQ7S/Glp8CDK0FHSSLEWlypMdpiPerBs23gk/xsbQjT31RHVd1iq4YVMqqLbYiRRA0AnDPQohV2zFBBwMBK5JchWjB47muK5uiHL2l/JvkepuraSTviQNaPxMjKM7z/jwdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'f09490cd01d030f3bed923ab84c529cd']);
+$response = $bot->getProfile('<userId>');
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
+
+
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
