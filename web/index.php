1b5ae9b9728245d462b4f3947bbf5213a0bef6af
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-08 10:46:24 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -139,8 +139,6 @@ foreach ($client->parseEvents() as $event) {
 			  
 		    case "image" :
 			$type=$message['type'];
-			$originalContentUrl=$message['originalContentUrl'];
-			$previewImageUrl=$message['previewImageUrl'];
                 	if($m_message!="")
                 	{
                 		$client->replyMessage(array(
@@ -148,7 +146,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $type . "\n" . $originalContentUrl . "\n" . $previewImageUrl
+                                'text' => $type
                             ),
                         ),
                     	));
