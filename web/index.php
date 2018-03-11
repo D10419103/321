6bac2048df59d644b25328ab060f76b8c3cecefc
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-11 20:00:53 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -52,10 +52,23 @@ foreach ($client->parseEvents() as $event) {
                             )	
                         )
                     	));
-$file='/app/web/output.txt'; 
-$myfile = fopen($file, "a");
-fwrite($myfile, "123");
-fclose($myfile);
+fwrite($file, $m_message."\n");
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $m_message);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$channelAccessToken
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+fwrite($file, $result."\n"); 
+fclose($file);
+curl_close($ch);
                 	}else if($m_message=="123"){
 				
 				$client->replyMessage(array(
