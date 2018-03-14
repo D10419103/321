7fc04f42a43e0f26208370ba0554f43630b8b6d0
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-14 16:25:59 +0800

    Update index01.php

--- web/index01.php
+++ web/index01.php
@@ -172,17 +172,15 @@ $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                 		$client->replyMessage(array(
                                     'replyToken' => $event['replyToken'],
                                     'messages' => array(
-                                      /* array(
+                                       array(
                                           'type' => 'text',
                                           'text' => $type . "\n" . $displayname
-                                       )*///,
+                                       ),
 				       array(
 					  
-                                          //'type' => 'image', // 訊息類型 (圖片)
-                                          //'originalContentUrl' => $originalContentUrl, // 回復圖片
-                                          //'previewImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg' // 回復的預覽圖片
-                                          'type' => 'text',
-                                          'text' => $type . "\n" . $originalContentUrl
+                                          'type' => 'image', // 訊息類型 (圖片)
+                                          'originalContentUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 回復圖片
+                                          'previewImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg' // 回復的預覽圖片
 				       ),
                                    ),
                                ));
