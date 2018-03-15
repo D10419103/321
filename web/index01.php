be0324e26e5b8ba6ff9fc31e8947b171091d94b0
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-15 11:24:07 +0800

    Update index01.php

--- web/index01.php
+++ web/index01.php
@@ -46,10 +46,8 @@ $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $text = $content->text;*/
 
 foreach ($client->parseEvents() as $event) {
-	$source=$event['source'];
-              	      	$userId=$source['userId'];
 	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage($userId, $textMessageBuilder);
+$response = $bot->pushMessage('U8acc7f611c6f853ac53e1a474bd77c92', $textMessageBuilder);
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];		
