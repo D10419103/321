6137dfd1425937e8f6b19c541636d48604a6ce1f
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-20 16:50:34 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -37,7 +37,7 @@ foreach ($client->parseEvents() as $event) {
 		    }
                     if($m_message=="安安"){
                        error_log("this");
-				 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($cool . "\n" . $m_message);
+				 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder( "\n" . $m_message);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 		    }
                     break;
