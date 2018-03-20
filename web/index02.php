214a3858ede4e914bee09afa566791f5be5b0313
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-20 16:22:02 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -11,13 +11,49 @@ require_once __DIR__ . '/../scr/LINEBot/MessageBuilder.php';
 require_once __DIR__ . '/../scr/LINEBot/HTTPClient/CurlHTTPClient.php';
 require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TextMessageBuilder.php';
 
-$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
+/*$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-
+*/
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret ]);
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
+//$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$receive = json_decode(file_get_contents("php://input"));
+	
+	// 讀取收到的訊息內容
+	$text = $receive->events[0]->message->text;
+	
+	// 讀取訊息來源的類型 	[user, group, room]
+	$type = $receive->events[0]->source->type;
+	
+	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
+	// 所以在這裡先判斷訊息的來源
+	if ($type == "room")
+	{
+		// 多人聊天 讀取房間id
+		$from = $receive->events[0]->source->roomId;
+	} 
+	else if ($type == "group")
+	{
+		// 群組 讀取群組id
+		$from = $receive->events[0]->source->groupId;
+	}
+	else
+	{
+		// 一對一聊天 讀取使用者id
+		$from = $receive->events[0]->source->userId;
+	}
+	
+	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
+	$content_type = $receive->events[0]->message->type;
+	
+	// 準備Post回Line伺服器的資料 
+	$header = ["Content-Type: application/json", "Authorization: Bearer {" . $channel_access_token . "}"];
+	
+	// 回覆訊息
+	reply($content_type, $text);
+
+function reply($content_type, $message) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
