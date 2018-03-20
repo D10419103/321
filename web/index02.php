76dc6190e15fa481e25c4d357caf255a75a47992
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-20 17:13:45 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -28,11 +28,23 @@ foreach ($client->parseEvents() as $event) {
                     date_default_timezone_set('Asia/Taipei');
                     $debugmsg='123456';
                     $mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
-                    $cool=mysqli_query($mysqli,'select Q from test where cool=456');
+                    $result=mysqli_query($mysqli,'select Q from test where cool=456');
+		$cool='';
+		//$result = $db->query("call getUsers()");
+		if($result){
+		     // Cycle through results
+		    while ($row = $result->fetch_object()){
+			$cool = $row;
+		    }
+		    // Free result set
+		    $result->close();
+		}    
 	            if(mysqli_connect_errno()){ 
                         $debugmsg='資料庫連線失敗';
                     }
                     else{
+			    
+			    
 			 $mysqli->close();
 		    }
                     if($m_message=="安安"){
