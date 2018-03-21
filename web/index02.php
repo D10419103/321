b7735833456882db774b2b770c6fb6acd5db6942
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-21 20:45:36 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -62,7 +62,7 @@ while($row = $result->fetch_array()) {
 				 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($cool. "\n" . $m_message);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);	  
 			    $mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
-			     "INSERT INTO test (Q,cool,test) VALUES ('111','222','333')";
+			    INSERT users SET username='Q',password='111';
 			     $mysqli->close();
 		    }
                     break;
