eb25e21a0170de1b5bf77518cda08e26b904c0f1
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-25 20:09:35 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -155,9 +155,10 @@ $bot->replyMessage($replyToken,$msg);*/
                         array(
                             'type' => 'message', // 類型 (訊息)
                             'label' => '上班', // 標籤 1
-                            'text' => '上班', $mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
-				$sql = "UPDATE workPunch SET worktype='上班' where name='$displayname' and worktype='';";
-			    $result = $mysqli->query($sql);// 用戶發送文字 1
+                            'text' => '上班', 
+				$mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306"),
+				$sql = "UPDATE workPunch SET worktype='上班' where name='$displayname' and worktype='';",
+			    $result = $mysqli->query($sql)// 用戶發送文字 1
                         ),
                         array(
                             'type' => 'message', // 類型 (訊息)
