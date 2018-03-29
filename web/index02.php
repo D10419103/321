5861188471fe93217124c0883e705c90332e200f
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-03-29 20:51:46 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -165,7 +165,7 @@ while($row = $result->fetch_array(MYSQLI_BOTH)) {
 				$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($type . "\n" . $m_message . "\n". $longitude . "\n" . $latitude ."\n". $userId ."\n" . $displayname);
 $bot->replyMessage($replyToken,$msg);
 			    }*/
-                	if($m_message!="" && sprintf("%.2f",substr(sprintf("%.3f", $longitude), 0, -2))=="121.50" && sprintf("%.2f",substr(sprintf("%.3f", $latituderound), 0, -2))=="24.99")
+                	if($m_message!=""/* && $longitude=="121.50" && $latituderound=="24.99"*/)
                 	{
 				/*$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($type . "\n" . $m_message . "\n". $longitude . "\n" . $latitude ."\n". $userId ."\n" . $displayname);
 $bot->replyMessage($replyToken,$msg);*/
