212c92ef0e7972629e7592f7de2c198410490901
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-01 21:01:29 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -57,10 +57,6 @@ foreach ($client->parseEvents() as $event) {
                     date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
 		    //if($m_message!="" && $userid!='Ud9a4e29db28b8b07a78cecf6d8ec3bdb' && $roomid!='R8466f385da9bd8eac6fb509622c0a892'){
 	            if($m_message!=""){
-		    	$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-		    	$sql = "SELECT userid from mysql";
-		    	$result = $mysqli->query($sql);
-			$row = $result->fetch_array(MYSQLI_BOTH);
 		    	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_message);
 		    	$response = $bot->pushMessage('U8acc7f611c6f853ac53e1a474bd77c92', $textMessageBuilder);
 			$response = $bot->pushMessage('U0da0177d489bff17a4d77614a0b23257', $textMessageBuilder);
