bac9543c3fe8078e53fe54f90eae3d75e1c76039
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-01 19:43:26 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -161,11 +161,11 @@ while($row = $result->fetch_array(MYSQLI_BOTH)) {
 			    $response = $bot->getProfile($userId);
 			    $profile = $response->getJSONDecodedBody();
 			    $displayname=$profile['displayName'];
-			    /*if($address!="")
+			    if($address!="" && $longitude=="/^121.500/" && $latituderound=="/^24.99/")
                 	{
 				$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($type . "\n" . $address . "\n". $longitude . "\n" . $latitude ."\n". $userId ."\n" . $displayname);
 $bot->replyMessage($replyToken,$msg);
-			    }*/
+			    }
                 	if($address!="" && $longitude=="/^121.500/" && $latituderound=="/^24.99/")
                 	{
 				/*$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($type . "\n" . $address . "\n". $longitude . "\n" . $latitude ."\n". $userId ."\n" . $displayname);
