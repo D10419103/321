95325175994580906e824c8ecb9a32fc1f29c6ed
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-01 20:23:56 +0800

    Update index02.php

--- web/index02.php
+++ web/index02.php
@@ -168,7 +168,7 @@ $bot->replyMessage($replyToken,$msg);
 			    }*/
 			    $a="/^121.500/";
 			    $b="/^24.99/";
-                	if($address!="" && $longitude==$a && $latituderound==$b)
+                	if($address!="" && $longitude=$a && $latituderound=$b)
                 	{
 				/*$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($type . "\n" . $address . "\n". $longitude . "\n" . $latitude ."\n". $userId ."\n" . $displayname);
 $bot->replyMessage($replyToken,$msg);*/
