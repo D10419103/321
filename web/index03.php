656faf0b5e0856efa587321a2421674bc9b6ec63
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-08 20:06:39 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 /**
  * Copyright 2016 LINE Corporation
@@ -15,55 +15,169 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 require_once('./LINEBotTiny.php');
-require_once __DIR__ . '/../scr/LINEBot.php';
-require_once __DIR__ . '/../scr/LINEBot/Response.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../scr/LINEBot/HTTPClient.php';
-require_once __DIR__ . '/../scr/LINEBot/MessageBuilder.php';
-require_once __DIR__ . '/../scr/LINEBot/TemplateActionBuilder.php';
 require_once __DIR__ . '/../scr/LINEBot/Constant/Meta.php';
-require_once __DIR__ . '/../scr/LINEBot/Constant/MessageType.php';
-require_once __DIR__ . '/../scr/LINEBot/Constant/ActionType.php';
-require_once __DIR__ . '/../scr/LINEBot/Constant/TemplateType.php';
-require_once __DIR__ . '/../scr/LINEBot/HTTPClient/Curl.php';
+require_once __DIR__ . '/../scr/LINEBot.php';
 require_once __DIR__ . '/../scr/LINEBot/HTTPClient/CurlHTTPClient.php';
+require_once __DIR__ . '/../scr/LINEBot/HTTPClient/Curl.php';
+require_once __DIR__ . '/../scr/LINEBot/Response.php';
+
+require_once __DIR__ . '/../scr/LINEBot/Constant/MessageType.php';
+require_once __DIR__ . '/../scr/LINEBot/MessageBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TextMessageBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/StickerMessageBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/LocationMessageBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/MultiMessageBuilder.php';
+
 require_once __DIR__ . '/../scr/LINEBot/TemplateActionBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/Constant/ActionType.php';
 require_once __DIR__ . '/../scr/LINEBot/TemplateActionBuilder/PostbackTemplateActionBuilder.php';
 require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TemplateBuilder.php';
-require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/LocationMessageBuilder.php';
+require_once __DIR__ . '/../scr/LINEBot/Constant/TemplateType.php';
 require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TemplateBuilder/ConfirmTemplateBuilder.php';
-require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TextMessageBuilder.php';
 require_once __DIR__ . '/../scr/LINEBot/MessageBuilder/TemplateMessageBuilder.php';
-
 
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret ]);
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
-            $message = $event['message'];
+            $message = $event['message'];		
             switch ($message['type']) {
-		  case 'text':
-		    $replyToken=$event['replyToken'];
-                    $m_message = $message['text']; $source=$event['source']; $idtype = $source['type'];  $userid=$source['userId'];
-                    $roomid=$source['roomId']; $groupid=$source['groupId'];
-                    $res = $bot->getProfile($userid); $profile = $res->getJSONDecodedBody();$displayName = $profile['displayName'];
-		    $address=$message['address']; $title=$message['title'];
-                    $longitude=$message['longitude']; $latitude=$message['latitude']; 
-                    date_default_timezone_set('Asia/Taipei');$time=date("Y-m-d H:i:s");
-		    //if($m_message!="" && $userid!='Ud9a4e29db28b8b07a78cecf6d8ec3bdb' && $roomid!='R8466f385da9bd8eac6fb509622c0a892'){
-	            if($m_message!=""){
-		    	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_message);
-		    	$response = $bot->pushMessage('U8acc7f611c6f853ac53e1a474bd77c92', $textMessageBuilder);
-			$response = $bot->pushMessage('U0da0177d489bff17a4d77614a0b23257', $textMessageBuilder);
-			    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($userid);
-			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+		    case 'text':
+                	$m_message = $message['text'];
+			$type = $message['type'];
+                	$source=$event['source'];
+              	      	$userId=$source['userId'];			
+                  	$roomid=$source['roomId'];
+             	       	$groupid=$source['groupId'];
+			$replyToken=$event['replyToken'];
+			    $type2=$event['type'];
+			    $timestamp=$event['timestamp'];
+			    $response = $bot->getProfile($userId);
+			    $profile = $response->getJSONDecodedBody();
+			    $displayname=$profile['displayName'];
+			date_default_timezone_set('Asia/Taipei');	   
+			    $time=date("Y-m-d H:i:s");
+				if($m_message=="進"){
+			$client->replyMessage(array(
+        		'replyToken' => $event['replyToken'],
+     			   'messages' => array(
+				   array(
+                                          'type' => 'text',
+                                          'text' => "歡迎你的到來!!" . "\n" . "祝你使用愉快!!"
+                                       ),
+       			     array(
+				'type' => 'sticker',
+				'stickerId' => 106,
+				'packageId' => 1
+         	   ),
+ 	       ),
+	    ));
+		 $mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
+				$sql = "UPDATE workPunch SET worktype='進' where name='$displayname' and worktype='';";
+			    $result = $mysqli->query($sql);
+		    }else if($m_message=="出"){
+			$client->replyMessage(array(
+        		'replyToken' => $event['replyToken'],
+     			   'messages' => array(
+				   array(
+                                          'type' => 'text',
+                                          'text' => "謝謝你的使用!!" . "\n" . "歡迎下次再來!!"
+                                       ),
+       			     array(
+				'type' => 'sticker',
+				'stickerId' => 13,
+				'packageId' => 1
+         	   ),
+ 	       ),
+	    ));
+			    $mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
+				$sql = "UPDATE workPunch SET worktype='出' where name='$displayname' and worktype='';";
+			    $result = $mysqli->query($sql);                  
 		    }
-		break;
+                    break;
+			    case 'location' :
+			    $replyToken=$event['replyToken'];
+			$source=$event['source'];
+              	      	$type = $source['type']; 
+              	      	$userId=$source['userId'];
+			$title=$message['title'];
+                   	$latitude=$message['latitude'];
+                   	$longitude=$message['longitude'];
+			$address = $message['address'];
+			$type=$message['type'];
+			    $m_message = $message['text'];
+			    date_default_timezone_set('Asia/Taipei');	   
+			    $time=date("Y-m-d H:i:s");
+			    $response = $bot->getProfile($userId);
+			    $profile = $response->getJSONDecodedBody();
+			    $displayname=$profile['displayName'];
+			    $c=$longitude;
+			    $e="/^121.5/";
+			    $b="/^24.99/";
+                	if($address!="" && $c=$e && $latituderound=$b)
+                	{
+				$mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
+			    $sql = "select number from workPunch";
+		$result = $mysqli->query($sql);
+			    
+		while($row = $result->fetch_array(MYSQLI_BOTH)) {
+  			$a = $row['number'] ;
+ 		 }
+			   $a+=1;
+				$sql="INSERT INTO workPunch (number,name,userid,location,longitude,latitude,worktime) VALUES ('$a','$displayname','$userId','$address','$longitude','$latitude','$time')";
+			    $result = $mysqli->query($sql);
+				
+				
+				$client->replyMessage(array(
+  'replyToken' => $event['replyToken'],
+    'messages' => array(
+            array(
+                'type' => 'template', // 訊息類型 (模板)
+                'altText' => 'Example confirm template', // 替代文字
+                'template' => array(
+                    'type' => 'confirm', // 類型 (確認)
+                    'text' => '你現在是要進還是出?', // 文字
+                    'actions' => array(
+                        array(
+                            'type' => 'message', // 類型 (訊息)
+                            'label' => '進', // 標籤 1
+                            'text' => '進'// 用戶發送文字 1
+                        ),
+                        array(
+                            'type' => 'message', // 類型 (訊息)
+                            'label' => '出', // 標籤 2
+                            'text' => '出' // 用戶發送文字 2
+                        )
+                    )
+                )
+            )
+        )
+    ));
+				sleep(30);
+				$sql = "select name from workPunch where worktype='' and userid='$userId'";
+			$result = $mysqli->query($sql);
+			while($row = $result->fetch_array(MYSQLI_BOTH)) {
+  			$name = $row['name'] ;
+			}	
+				if($name!=""){
+				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請按進出按紐");
+		    		$response = $bot->pushMessage('U1bfd8c42263e43bc3f34a6d0c4e1ecb2', $textMessageBuilder);
+				}
+			
+				
+			}
+                    break;
+			
             }
             break;
         default:
