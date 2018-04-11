5f11a2ec747ed5e6b1d532eafad4bf38bad5ec6c
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-11 16:35:59 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -135,7 +135,7 @@ foreach ($client->parseEvents() as $event) {
 					$result = $mysqli->query($sql);
 					$row = $result->fetch_array(MYSQLI_BOTH);
 					$outside = $row['outside'] ;
-					else if(preg_match("/$outside/i","$m_message")){
+					if(preg_match("/$outside/i","$m_message")){
 						$mysqli->close();
 					//else if($m_message=="出"){
 						$mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy", "fu7wm9fyq2nkgeuk","3306");
