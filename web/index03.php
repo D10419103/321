300ff48ebe77cd3d416630b7587b11896dfa0fa2
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-11 17:02:47 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -53,7 +53,7 @@ foreach ($client->parseEvents() as $event) {
 			    		$time=date("Y-m-d H:i:s");
 					
 					$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
-					$sql = "SELECT inside from inandout";
+					$sql = "SELECT inside from inin";
 					$result = $mysqli->query($sql);
 					$row = $result->fetch_array(MYSQLI_BOTH);
 					$inside = $row['inside'] ;
