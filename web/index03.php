f3811a525c26423ae6d3363a4b5d33ceaa219c4f
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-11 16:32:41 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -128,7 +128,9 @@ foreach ($client->parseEvents() as $event) {
 							}
 
 						}
-					}$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
+						$mysqli->close();
+					}
+					$mysqli = new mysqli('gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "vu5qzklum1466fvr", "ieewar6pa07471zn", "oqz0qx1hdl6jbtca","3306");
 					$sql = "SELECT outside from inandout";
 					$result = $mysqli->query($sql);
 					$row = $result->fetch_array(MYSQLI_BOTH);
