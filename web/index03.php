077c3e97f37d4e4a706a84781295fdd36525e885
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-11 15:50:29 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -117,7 +117,6 @@ foreach ($client->parseEvents() as $event) {
 								if($name!=""){
 									$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請定位你的位置");
 									$response = $bot->pushMessage($userId, $textMessageBuilder);
-									break;
 								}
 							}
 						}
@@ -186,6 +185,7 @@ foreach ($client->parseEvents() as $event) {
 							if($name!=""){
 								$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請定位你的位置");
 								$response = $bot->pushMessage($userId, $textMessageBuilder);
+							}else{
 								break;
 							}
 						}
@@ -279,7 +279,6 @@ foreach ($client->parseEvents() as $event) {
 								if($name!=""){
 									$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請按進出按紐");
 									$response = $bot->pushMessage($userId, $textMessageBuilder);
-									break;
 								}
 							}
 				    		}
