c2c0ac4d25992228198da984124510c1b24f1063
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-12 20:20:53 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -239,15 +239,15 @@ foreach ($client->parseEvents() as $event) {
 											array(
 												'type' => 'message', // 類型 (訊息)
 												'label' => 'in', // 標籤 1
-												'text' => '進', // 用戶發送文字
+												'text' => '設置成功', // 用戶發送文字
 												$mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy","fu7wm9fyq2nkgeuk","3306"),
-												$sql="INSERT INTO workPunch (inside) VALUES ('$m_message')",
+												$sql="INSERT INTO ininin (inside) VALUES ('$m_message')",
 												$result = $mysqli->query($sql)
 											),
 											array(
 												'type' => 'message', // 類型 (訊息)
 												'label' => 'out', // 標籤 2
-												'text' => '出' // 用戶發送文字
+												'text' => '設置成功' // 用戶發送文字
 											),
 											array(
 												'type' => 'message', // 類型 (訊息)
