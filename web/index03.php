86b4e7333cd58fcb4932e7a8da73f1631fc5d2ee
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-12 18:00:34 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -147,7 +147,7 @@ foreach ($client->parseEvents() as $event) {
 									$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("請定位你的位置");
 									$response = $bot->pushMessage($userId, $textMessageBuilder);
 								}
-
+							}
 						}
 					}else if($unjoin){
 					//else if($m_message=="出"){
