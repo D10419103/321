b9ca58e1432298dcc21b94903fa25a804abe48e2
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-04-12 20:47:51 +0800

    Update index03.php

--- web/index03.php
+++ web/index03.php
@@ -240,7 +240,7 @@ foreach ($client->parseEvents() as $event) {
 												'type' => 'message', // 類型 (訊息)
 												'label' => '進', // 標籤 1
 												$mysqli = new mysqli('edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "ia8wipiqgptyg9yb", "ywz5dcdawbeq11cy","fu7wm9fyq2nkgeuk","3306"),
-												$sql="INSERT INTO USERINFO ininin (inside) VALUES ('$m_message')",
+												$sql="INSERT INTO ininin (inside) VALUES ('$m_message')",
 												$result = $mysqli->query($sql),
 												$mysqli->close(),
 												'text' => $m_message // 用戶發送文字
