c5ba884e844dc222cfcd027fee635b456fe1e748
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-05-03 11:19:14 +0800

    Update index04.php

--- web/index04.php
+++ web/index04.php
@@ -62,7 +62,7 @@ foreach ($client->parseEvents() as $event) {
 						$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($key);
                         $bot->replyMessage($replyToken,$msg);
 			}$standard_A = "/^([0-9]+)$/"; 
-			if ($m_message== $numbercode ) {
+			if ($m_message== $numbercode && preg_match("/$numbercode/i", "$m_message")) {
 				$sql="INSERT INTO code (msg) VALUES ('ok')";
 							$result = $mysqli->query($sql);
 				$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("驗證成功");
@@ -70,7 +70,7 @@ foreach ($client->parseEvents() as $event) {
 							$sql="delete from code where numbercode='$m_message'";
 							$result = $mysqli->query($sql);	
 					
-					}else if (preg_match("/123456789/i", "$m_message")) {
+					}else {
 				$msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("驗證失敗");
                         $bot->replyMessage($replyToken,$msg);
 			}
